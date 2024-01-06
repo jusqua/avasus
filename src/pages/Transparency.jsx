28320ecfc846941a422e011bc335c334116b37c7
Ádrian Gama <adrian.gama.se@gmail.com>
2024-01-06 20:37:26 -0300

    Simplify general data display logic

--- src/pages/Transparency.jsx
+++ src/pages/Transparency.jsx
@@ -12,18 +12,6 @@ import {
 import instance from '@utils/api';
 import Breadcrumbs from '@components/Breadcrumbs';
 
-function GeneralDataItem({ icon, title, content }) {
-  return (
-    <div className="flex flex-col gap-1 justify-center text-center">
-      <span className="flex gap-1 items-center justify-center">
-        {icon}
-        <p className="text-sm">{title}</p>
-      </span>
-      <p className="text-2xl text-primary">{content}</p>
-    </div>
-  );
-}
-
 function Transparency() {
   const [data, setData] = useState({});
   const [loaded, setLoaded] = useState(false);
@@ -48,86 +36,94 @@ function Transparency() {
       <h1 className="text-4xl text-primary text-center my-4">Transparência</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 grid-flow-row-dense gap-6 *:rounded-2xl *:bg-base-200 *:shadow-md">
         <div className="flex md:col-span-2 min-h-64">
-          {!loaded ? (
-            <div className="skeleton w-full h-full"></div>
-          ) : (
-            <div className="flex flex-col flex-1 justify-evenly m-6 items-center">
-              <h2 className="text-2xl text-primary p-2">Dados Gerais</h2>
-              <div className="grid items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-flow-row-dense gap-3 flex-1 w-full">
-                <IconContext.Provider
-                  value={{
-                    className: 'fill-primary stroke-primary',
-                    size: '20',
-                    weight: 'fill',
-                  }}
-                >
-                  <GeneralDataItem
-                    icon={<UsersFour />}
-                    title="Total de usuários registrados"
-                    content={data['dados_gerais']['usuarios_registrados']}
-                  />
-                  <GeneralDataItem
-                    icon={<FileArrowUp />}
-                    title="Inscrições realizadas"
-                    content={data['dados_gerais']['incricoes_realizadas']}
-                  />
-                  <GeneralDataItem
-                    icon={<GraduationCap />}
-                    title="Cursos ativos"
-                    content={data['dados_gerais']['cursos_ativos']}
-                  />
-                  <GeneralDataItem
-                    icon={<Certificate />}
-                    title="Direito à certificação"
-                    content={data['dados_gerais']['direito_certificacao']}
-                  />
-                  <GeneralDataItem
-                    icon={
-                      <div className="relative">
-                        <GraduationCap />
-                        <CurrencyDollar
-                          className="fill-primary stroke-primary absolute -top-2 -left-2"
-                          size="14"
-                        />
+          <div className="flex flex-col flex-1 justify-evenly m-6 items-center">
+            <h2 className="text-2xl text-primary p-2">Dados Gerais</h2>
+            <div className="grid items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-flow-row-dense gap-3 flex-1 w-full">
+              <IconContext.Provider
+                value={{
+                  className: 'fill-primary stroke-primary',
+                  size: '20',
+                  weight: 'fill',
+                }}
+              >
+                <GeneralDataItem
+                  loaded={loaded}
+                  title="Total de usuários registrados"
+                  content={data.dados_gerais?.usuarios_registrados}
+                  icon={<UsersFour />}
+                />
+                <GeneralDataItem
+                  loaded={loaded}
+                  title="Inscrições realizadas"
+                  content={data.dados_gerais?.incricoes_realizadas}
+                  icon={<FileArrowUp />}
+                />
+                <GeneralDataItem
+                  loaded={loaded}
+                  title="Cursos ativos"
+                  content={data.dados_gerais?.cursos_ativos}
+                  icon={<GraduationCap />}
+                />
+                <GeneralDataItem
+                  loaded={loaded}
+                  title="Direito à certificação"
+                  content={data.dados_gerais?.direito_certificacao}
+                  icon={<Certificate />}
+                />
+                <GeneralDataItem
+                  loaded={loaded}
+                  title="Investimento médio por curso"
+                  content={data.dados_gerais?.investimento_medio_curso}
+                  icon={
+                    <div className="relative">
+                      <GraduationCap />
+                      <div className="absolute -top-2 -left-2">
+                        <CurrencyDollar size="14" />
                       </div>
-                    }
-                    title="Investimento médio por curso"
-                    content={data['dados_gerais']['investimento_medio_curso']}
-                  />
-                  <GeneralDataItem
-                    icon={
-                      <div className="relative">
-                        <User />
-                        <CurrencyDollar
-                          className="fill-primary stroke-primary absolute -top-2 -left-2"
-                          size="14"
-                        />
+                    </div>
+                  }
+                />
+                <GeneralDataItem
+                  loaded={loaded}
+                  title="Investimento médio por aluno"
+                  content={data.dados_gerais?.investimento_medio_aluno}
+                  icon={
+                    <div className="relative">
+                      <User />
+                      <div className="absolute -top-2 -left-2">
+                        <CurrencyDollar size="14" />
                       </div>
-                    }
-                    title="Investimento médio por aluno"
-                    content={data['dados_gerais']['investimento_medio_aluno']}
-                  />
-                </IconContext.Provider>
-              </div>
+                    </div>
+                  }
+                />
+              </IconContext.Provider>
             </div>
-          )}
+          </div>
         </div>
         <div className="min-h-[36rem]">
-          {!loaded ? (
-            <div className="skeleton w-full h-full"></div>
-          ) : (
-            <div></div>
-          )}
+          <div></div>
         </div>
         <div className="min-h-[36rem]">
-          {!loaded ? (
-            <div className="skeleton w-full h-full"></div>
-          ) : (
-            <div></div>
-          )}
+          <div></div>
         </div>
       </div>
     </>
+  );
+}
+
+function GeneralDataItem({ loaded, title, content, icon }) {
+  return (
+    <div className="flex flex-col gap-1 justify-center text-center">
+      <span className="flex gap-1 items-center justify-center">
+        {icon}
+        <p className="text-sm">{title}</p>
+      </span>
+      {!loaded ? (
+        <div className="skeleton h-8 w-full"></div>
+      ) : (
+        <p className="text-2xl text-primary">{content}</p>
+      )}
+    </div>
   );
 }
 
