e6e618e3dff4486dad8929fcac1d04bc08a00e20
Ádrian Gama <adrian.gama.se@gmail.com>
2024-01-08 18:56:22 -0300

    Better `Error` messages

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,17 +1,21 @@
 import { SmileyXEyes } from '@phosphor-icons/react';
+import { useRouteError } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function Error() {
+  const error = useRouteError();
+
   return (
     <div className="flex flex-col my-16 gap-4 justify-center items-center">
       <SmileyXEyes size="256" />
       <h1 className="text-2xl md:text-8xl text-center">
         Desculpe-nos<i>!</i>
       </h1>
-      <p className="text-sm md:text-2xl text-center">
-        A página que está procurando não existe
+      <p className="text-sm md:text-2xl text-center">Algo deu errado</p>
+      <p className="text-sm md:text-2xl text-center bg-base-200 p-4 rounded-xl">
+        {error.statusText || error.message}
       </p>
-      <Link className="btn btn-lg btn-warning" to="/">
+      <Link className="btn btn-md btn-warning" to="/">
         Me tire daqui
       </Link>
     </div>
