4b89b0cacf46a081e9887132664b7ad9d15559a7
Ádrian Gama <adrian.gama.se@gmail.com>
2024-01-09 20:43:30 -0300

    Add `size` property to `Rating`

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,6 @@
 import { Star, StarHalf } from '@phosphor-icons/react';
 
-function Rating({ rating }) {
+function Rating({ rating, size = '20' }) {
   const full = Number.parseInt(rating);
   const half = +(rating - full >= 0.5);
   const empty = 5 - half - full;
@@ -8,13 +8,13 @@ function Rating({ rating }) {
   return (
     <div className="flex gap-1">
       {[...Array(full).keys()].map((e) => (
-        <Star size="20" weight="fill" className="fill-primary" key={e} />
+        <Star size={size} weight="fill" className="fill-primary" key={e} />
       ))}
       {!half ? null : (
-        <StarHalf size="20" weight="fill" className="fill-primary" />
+        <StarHalf size={size} weight="fill" className="fill-primary" />
       )}
       {[...Array(empty).keys()].map((e) => (
-        <Star size="20" key={e} />
+        <Star size={size} key={e} />
       ))}
     </div>
   );
