5a98b229a3111b21809f0187550659c071db0a3b
Ádrian Gama <adrian.gama.se@gmail.com>
2024-01-11 15:09:07 -0300

    Add `Breadcrumbs` title extra link and skeleton

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,10 +1,17 @@
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useMatches } from 'react-router-dom';
 
-function Breadcrumbs() {
+function Breadcrumbs({ title, loaded = true }) {
+  const location = useLocation();
   const matches = useMatches();
   const crumbs = matches
     .filter((match) => Boolean(match.handle?.crumb))
-    .map((match) => match.handle.crumb(match.data));
+    .map((match) => match.handle.crumb());
+
+  if (title) crumbs.push(<Link to={location.pathname}>{title}</Link>);
+
+  if (!loaded) return <div className="max-w-xs h-4 my-2 skeleton"></div>;
 
   return (
     <div className="max-w-xs text-sm breadcrumbs my-2">
