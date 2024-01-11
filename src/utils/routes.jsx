f85f375d5c9fd702f72d007bc6956d9265579d89
Ádrian Gama <adrian.gama.se@gmail.com>
2024-01-11 14:47:03 -0300

    Refact `routes` logic

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -1,40 +1,71 @@
-import { Link } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 import Home from '@pages/Home';
 import Courses from '@pages/Courses';
 import Partners from '@pages/Partners';
 import Transparency from '@pages/Transparency';
+import Module from '@pages/Module';
 
 import Body from '@components/Body';
 import WorkInProgress from '@components/WorkInProgress';
 import Error from '@components/Error';
 
-function createRoute(title, path, element, children, errorElement) {
+function createRoute({
+  title,
+  path,
+  element,
+  errorElement,
+  index = false,
+  crumb = true,
+  children,
+}) {
+  const handle = { title };
+
+  if (!index && crumb && title)
+    handle.crumb = () => <Link to={path}>{title}</Link>;
+
+  if (!element) element = children ? <Outlet /> : <WorkInProgress />;
+
   return {
-    title,
     path,
     element,
-    children,
-    handle: { crumb: () => <Link to={path}>{title}</Link> },
     errorElement,
+    index,
+    handle,
+    children,
   };
 }
 
 const routes = [
-  createRoute(
-    'Início',
-    '/',
-    <Body />,
-    [
-      createRoute('Início', '/', <Home />),
-      createRoute('Sobre Nós', '/about', <WorkInProgress />),
-      createRoute('Cursos', '/courses', <Courses />),
-      createRoute('Parceiros', '/partners', <Partners />),
-      createRoute('Transparência', '/transparency', <Transparency />),
-      createRoute('Contato', '/contact', <WorkInProgress />),
+  createRoute({
+    title: 'Início',
+    path: '/',
+    element: <Body />,
+    errorElement: <Error />,
+    children: [
+      createRoute({ index: true, element: <Home /> }),
+      createRoute({ title: 'Sobre Nós', path: '/about' }),
+      createRoute({
+        title: 'Cursos',
+        path: '/courses',
+        children: [
+          createRoute({ index: true, element: <Courses /> }),
+          createRoute({ path: '/courses/:id', element: <Module /> }),
+        ],
+      }),
+      createRoute({
+        title: 'Parceiros',
+        path: '/partners',
+        element: <Partners />,
+      }),
+      createRoute({
+        title: 'Transparência',
+        path: '/transparency',
+        element: <Transparency />,
+      }),
+      createRoute({ title: 'Contato', path: '/contact' }),
     ],
-    <Error />,
-  ),
+  }),
 ];
 
 export default routes;
