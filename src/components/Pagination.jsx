9f12895bea035c13690b8b229fae6a5db2cc3076
Ádrian Gama <adrian.gama.se@gmail.com>
2024-01-12 11:02:35 -0300

    Fix bad `Pagination` logic and move component downwards

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,116 @@
 import { DotsThree, CaretLeft, CaretRight } from '@phosphor-icons/react';
 
+function Pagination({
+  index,
+  setIndex,
+  length,
+  multiplier,
+  limit,
+  loaded = true,
+  children,
+}) {
+  function handleClick(index) {
+    return () => setIndex(index);
+  }
+
+  if (!loaded)
+    return (
+      <div className="flex flex-col gap-1">
+        <div className="skeleton h-4 w-64"></div>
+        <div className="self-center md:hidden skeleton w-72 h-8"></div>
+        <div className="flex-grow-1">{children}</div>
+        <div className="self-center skeleton w-72 h-8"></div>
+      </div>
+    );
+
+  return (
+    <div className="flex flex-col">
+      <i className="text-gray-500 font-normal">
+        {Math.min((index + 1) * multiplier, limit)} de {limit} resultados
+      </i>
+      <div className="self-center md:hidden">
+        <PaginationItem
+          index={index}
+          length={length}
+          handleClick={handleClick}
+        />
+      </div>
+      <div className="flex-grow-1">{children}</div>
+      <div className="self-center">
+        <PaginationItem
+          index={index}
+          length={length}
+          handleClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+}
+
 const maxPaginationItems = 5;
 const firstCountLimit = Math.round(maxPaginationItems / 2);
+
+function PaginationItem({ index, length, handleClick }) {
+  return (
+    <div className="join my-2">
+      <button
+        className="join-item btn btn-sm btn-square"
+        disabled={index === 0}
+        onClick={handleClick(Math.max(0, index - 1))}
+      >
+        <CaretLeft weight="bold" />
+      </button>
+      <input
+        className="join-item btn btn-sm btn-square"
+        type="radio"
+        value={0}
+        aria-label={1}
+        onChange={handleClick(0)}
+        checked={index === 0}
+      />
+      {index < firstCountLimit + 1 ||
+        length === maxPaginationItems + 3 ? null : (
+        <button className="join-item btn btn-sm btn-square" disabled>
+          <DotsThree weight="bold" />
+        </button>
+      )}
+      {getPaginationList(index, length).map((e) => (
+        <input
+          key={e}
+          className="join-item btn btn-sm btn-square"
+          type="radio"
+          value={e}
+          aria-label={e + 1}
+          onChange={handleClick(e)}
+          checked={index === e}
+        />
+      ))}
+      {index >= length - firstCountLimit - 1 ||
+        length === maxPaginationItems + 3 ? null : (
+        <button className="join-item btn btn-sm btn-square" disabled>
+          <DotsThree weight="bold" />
+        </button>
+      )}
+      {length <= 1 ? null : (
+        <input
+          className="join-item btn btn-sm btn-square"
+          type="radio"
+          value={length - 1}
+          aria-label={length}
+          onChange={handleClick(length - 1)}
+          checked={index === length - 1}
+        />
+      )}
+      <button
+        className="join-item btn btn-sm btn-square"
+        disabled={index === length - 1}
+        onClick={handleClick(Math.min(index + 1, length - 1))}
+      >
+        <CaretRight weight="bold" />
+      </button>
+    </div>
+  );
+}
 
 function getPaginationList(index, length) {
   const after = [];
@@ -37,103 +146,6 @@ function getPaginationList(index, length) {
     before.push(i);
 
   return [...after.reverse(), ...before];
-}
-
-function Pagination({
-  index,
-  setIndex,
-  length,
-  multiplier,
-  limit,
-  loaded = true,
-  children,
-}) {
-  function handleClick(index) {
-    return () => setIndex(index);
-  }
-
-  function PaginationItem() {
-    return (
-      <div className="join my-2">
-        <button
-          className="join-item btn btn-sm btn-square"
-          disabled={index === 0}
-          onClick={handleClick(Math.max(0, index - 1))}
-        >
-          <CaretLeft weight="bold" />
-        </button>
-        <input
-          className="join-item btn btn-sm btn-square"
-          type="radio"
-          value={0}
-          aria-label={1}
-          onChange={handleClick(0)}
-          checked={index === 0}
-        />
-        {index < firstCountLimit + 1 ? null : (
-          <button className="join-item btn btn-sm btn-square" disabled>
-            <DotsThree weight="bold" />
-          </button>
-        )}
-        {getPaginationList(index, length).map((e) => (
-          <input
-            key={e}
-            className="join-item btn btn-sm btn-square"
-            type="radio"
-            value={e}
-            aria-label={e + 1}
-            onChange={handleClick(e)}
-            checked={index === e}
-          />
-        ))}
-        {index >= length - firstCountLimit - 1 ? null : (
-          <button className="join-item btn btn-sm btn-square" disabled>
-            <DotsThree weight="bold" />
-          </button>
-        )}
-        <input
-          className="join-item btn btn-sm btn-square"
-          type="radio"
-          value={length - 1}
-          aria-label={length}
-          onChange={handleClick(length - 1)}
-          checked={index === length - 1}
-        />
-        <button
-          className="join-item btn btn-sm btn-square"
-          disabled={index === length - 1}
-          onClick={handleClick(Math.min(index + 1, length - 1))}
-        >
-          <CaretRight weight="bold" />
-        </button>
-      </div>
-    );
-  }
-
-  if (!loaded)
-    return (
-      <div className="flex flex-col gap-1">
-        <div className="skeleton h-4 w-64"></div>
-        <div className="self-center md:hidden skeleton w-72 h-8"></div>
-        <div className="flex-grow-1">{children}</div>
-        <div className="self-center skeleton w-72 h-8"></div>
-      </div>
-    );
-
-  return (
-    <div className="flex flex-col">
-      <i className="text-gray-500 font-normal">
-        {Math.min((index + 1) * multiplier, limit)} de {limit} resultados
-      </i>
-      <div className="self-center md:hidden">
-        <PaginationItem />
-      </div>
-      <div className="flex-grow-1">{children}</div>
-      <div className="self-center">
-        <PaginationItem />
-      </div>
-    </div>
-  );
 }
 
 export default Pagination;
